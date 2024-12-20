ab84ae6b817b95cfde455c03fefa612ac509e707
jhakrishan20 <krishanjha80@gmail.com>
2024-12-20 23:38:46 +0530

    mistakes corrected

--- src/pages/education.jsx
+++ src/pages/education.jsx
@@ -14,7 +14,7 @@ function Education(){
                                         <div className="row align-items-center gx-5">
                                             <div className="col text-center text-lg-start mb-4 mb-lg-0">
                                                 <div className="bg-light p-4 rounded-4">
-                                                    <div className="text-secondary fw-bolder mb-2">2008 - 2021</div>
+                                                    <div className="text-secondary fw-bolder mb-2">2022 - 2026</div>
                                                     <div className="mb-2">
                                                         <div className="small fw-bolder">Galgotias University</div>
                                                         <div className="small text-muted">Greater Noida, UP</div>
@@ -43,7 +43,7 @@ function Education(){
                                         <div className="row align-items-center gx-5">
                                             <div className="col text-center text-lg-start mb-4 mb-lg-0">
                                                 <div className="bg-light p-4 rounded-4">
-                                                    <div className="text-secondary fw-bolder mb-2">2022 - 2026</div>
+                                                    <div className="text-secondary fw-bolder mb-2">2008 - 2021</div>
                                                     <div className="mb-2">
                                                         <div className="small fw-bolder">D.A.V public School</div>
                                                         <div className="small text-muted">Faridabad, Haryana</div>
